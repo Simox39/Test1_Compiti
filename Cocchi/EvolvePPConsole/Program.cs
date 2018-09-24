0f9858ec65d3a3e1af3ff5b86e03e428d214b8ce
Simone Cocchi <ilgrosso7516@gmail.com>
2018-09-24 20:20:54 +0200

    ho effettuato una modifica

---
+++ Cocchi/EvolvePPConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvolvePPConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
